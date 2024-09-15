c086d3a0490c6b867d60ac95933c7f468723b440
Mateus <mateusleandro2205@gmail.com>
2024-09-14 22:25:49 -0300

    Criado classe para autenticação dos usuários.

---
+++ lib/services/auth_service.dart
@@ -0,0 +1,66 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  Future<String?> entrarUsuario(
+      {required String email, required String senha}) async {
+    try {
+      await _firebaseAuth.signInWithEmailAndPassword(
+          email: email, password: senha);
+    } on FirebaseAuthException catch (e) {
+      return erroAuth(e);
+    }
+    return null;
+  }
+
+  Future<String?> cadastrarUsuario(
+      {required String email,
+      required String senha,
+      required String nome}) async {
+    try {
+      UserCredential userCredential = await _firebaseAuth
+          .createUserWithEmailAndPassword(email: email, password: senha);
+    } on FirebaseAuthException catch (e) {
+      return erroAuth(e);
+    }
+    return null;
+  }
+
+  Future<String?> redefinicaoSenha({required String email}) async {
+    try {
+      await _firebaseAuth.sendPasswordResetEmail(email: email);
+    } on FirebaseAuthException catch (e) {
+      return erroAuth(e);
+    }
+    return null;
+  }
+
+  Future<String?> deslogar() async {
+    try {
+      await _firebaseAuth.signOut();
+    } on FirebaseAuthException catch (e) {
+      return e.code;
+    }
+    return null;
+  }
+
+  Future<String?> erroAuth(FirebaseAuthException e) async {
+    switch (e.code) {
+      case 'user-not-found':
+        return 'Usuário não encontrado';
+      case 'wrong-password':
+        return 'Senha incorreta';
+      case 'invalid-email':
+        return 'Email Inválido/não encontrado.';
+      case 'email-already-in-use':
+        return 'O email já está em uso.';
+      case 'invalid-credential':
+        return 'Credenciais inválidas.';
+      case 'weak-password':
+        return 'Senha fraca';
+      default:
+        return 'Erro, tente novamente.';
+    }
+  }
+}
