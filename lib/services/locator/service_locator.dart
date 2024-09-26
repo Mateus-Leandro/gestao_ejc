d35fa09d807cd711ff212724f8598234164a1afd
Mateus <mateusleandro2205@gmail.com>
2024-09-26 17:49:40 -0300

    Registrado novas instancias no serviceLocator.

--- lib/services/locator/service_locator.dart
+++ lib/services/locator/service_locator.dart
@@ -1,6 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:gestao_ejc/controllers/user_controller.dart';
 import 'package:gestao_ejc/helpers/date_format_string.dart';
 import 'package:gestao_ejc/services/auth_service.dart';
+import 'package:gestao_ejc/services/user_service.dart';
 import 'package:get_it/get_it.dart';
 
 final getIt = GetIt.instance;
@@ -10,5 +13,10 @@ void setupGetIt() {
   getIt.registerLazySingleton<FirebaseAuth>(() => FirebaseAuth.instance);
   getIt.registerLazySingleton<DateFormatString>(
       () => DateFormatString(originalDate: DateTime.now()));
-
+  getIt.registerLazySingleton<FirebaseFirestore>(
+      () => FirebaseFirestore.instance);
+  getIt.registerLazySingleton<UserService>(
+      () => UserService());
+  getIt.registerLazySingleton<UserController>(
+      () => UserController());
 }
