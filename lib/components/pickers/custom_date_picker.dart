4e5da3b217a831f0dde2395a666a6a464fb848ed
Mateus <mateusleandro2205@gmail.com>
2024-10-30 15:33:39 -0300

    Melhoria no componente CustomDatePicker.

--- lib/components/pickers/custom_date_picker.dart
+++ lib/components/pickers/custom_date_picker.dart
@@ -1,16 +1,20 @@
+import 'package:calendar_date_picker2/calendar_date_picker2.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
+import 'package:gestao_ejc/functions/function_date.dart';
+import 'package:gestao_ejc/services/locator/service_locator.dart';
 
 class CustomDatePicker extends StatefulWidget {
   final TextEditingController controller;
   final String labelText;
   final DateTime? lowestDate;
+  final DateTime? higherDate;
 
   const CustomDatePicker({
     super.key,
     required this.controller,
     required this.labelText,
     this.lowestDate,
+    this.higherDate,
   });
 
   @override
@@ -18,41 +22,53 @@ class CustomDatePicker extends StatefulWidget {
 }
 
 class _CustomDatePickerState extends State<CustomDatePicker> {
-  DateTime? _selectedDate;
-
-  Future<void> _selectDate(BuildContext context) async {
-    final DateTime? picked = await showDatePicker(
-      context: context,
-      initialDate: _selectedDate ?? DateTime.now(),
-      firstDate: widget.lowestDate ?? DateTime(1900),
-      lastDate: DateTime.now(),
-      locale: const Locale('pt', 'BR'),
-    );
-    if (picked != null && picked != _selectedDate) {
-      setState(() {
-        _selectedDate = picked;
-        widget.controller.text =
-            DateFormat('dd/MM/yyyy', 'pt_BR').format(picked);
-      });
-    }
-  }
+  final FunctionDate functionDate = getIt<FunctionDate>();
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      controller: widget.controller,
-      readOnly: true,
-      decoration: InputDecoration(
-        labelText: widget.labelText,
-        border: const OutlineInputBorder(),
+    return GestureDetector(
+      onTap: _openDatePickerDialog,
+      child: AbsorbPointer(
+        child: TextFormField(
+          controller: widget.controller,
+          decoration: InputDecoration(
+            labelText: widget.labelText,
+            suffixIcon: const Icon(Icons.calendar_today),
+          ),
+          validator: (value) {
+            if (value == null || value.isEmpty) {
+              return 'Necessário informar a data';
+            }
+            return null;
+          },
+        ),
       ),
-      onTap: () => _selectDate(context),
-      validator: (value) {
-        if (value == null || value.isEmpty) {
-          return 'A data não pode ser vazia';
-        }
-        return null;
-      },
     );
+  }
+
+  Future<void> _openDatePickerDialog() async {
+    List<DateTime?> selectedDate = [];
+    String stringDate = widget.controller.text.trim();
+    if (stringDate.isNotEmpty) {
+      selectedDate.add(functionDate.getDateFromStringFormatted(stringDate));
+    }
+
+    final selectedDates = await showCalendarDatePicker2Dialog(
+      context: context,
+      config: CalendarDatePicker2WithActionButtonsConfig(
+        calendarType: CalendarDatePicker2Type.single,
+        firstDate: widget.lowestDate,
+        lastDate: widget.higherDate,
+      ),
+      dialogSize: const Size(325, 400),
+      value: selectedDate,
+    );
+
+    if (selectedDates != null && selectedDates.isNotEmpty) {
+      setState(() {
+        widget.controller.text =
+            functionDate.getDateToString(selectedDates[0]!);
+      });
+    }
   }
 }
