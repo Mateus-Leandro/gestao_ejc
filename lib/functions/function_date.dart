912130220de15098acb2903eb60023f7f11ce61c
Mateus <mateusleandro2205@gmail.com>
2024-10-30 13:23:28 -0300

    Retornar Datetime a parte de string e função para retornar diferença de dias entre datas.

--- lib/functions/function_date.dart
+++ lib/functions/function_date.dart
@@ -2,27 +2,38 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:intl/intl.dart';
 
 class FunctionDate {
+  final DateFormat dateFormat = DateFormat('dd/MM/yyyy');
+
   String getActualDateToString() {
-    return DateFormat('dd/MM/yyyy').format(DateTime.now());
+    return dateFormat.format(DateTime.now());
   }
 
   String getDateToString(DateTime date) {
-    return DateFormat('dd/MM/yyyy').format(date);
+    return dateFormat.format(date);
   }
 
   getTimestampFromString(String? stringDate) {
     return stringDate != null
-        ? Timestamp.fromDate(DateFormat('dd/MM/yyyy').parse(stringDate))
+        ? Timestamp.fromDate(dateFormat.parse(stringDate))
         : Timestamp.fromDate(DateTime.now());
   }
 
   String getStringFromTimestamp(Timestamp timestampDate) {
     DateTime date = DateTime.fromMillisecondsSinceEpoch(
         timestampDate.millisecondsSinceEpoch);
-    return DateFormat('dd/MM/yyyy').format(date);
+    return dateFormat.format(date);
   }
 
-  getTimestampFromDateTime(DateTime dateTime) {
+  Timestamp getTimestampFromDateTime(DateTime dateTime) {
     return Timestamp.fromDate(dateTime);
+  }
+
+  DateTime getDateFromStringFormatted(String dateFormatted) {
+    return dateFormat.parseStrict(dateFormatted);
+  }
+
+  int getDaysBetweenDates(
+      {required DateTime initialDate, required DateTime finalDate}) {
+    return finalDate.difference(initialDate).inDays;
   }
 }
