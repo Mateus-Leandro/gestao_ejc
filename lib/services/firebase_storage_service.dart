24e470c20bcdae4c1106b9044a87d9800a859980
mateus.chagas <mateus.chagas@avancoinfo.com.br>
2024-11-08 20:35:21 -0300

    :zap:perf: Função para comprimir imagem.

--- lib/services/firebase_storage_service.dart
+++ lib/services/firebase_storage_service.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/services.dart';
 import 'package:gestao_ejc/services/locator/service_locator.dart';
+import 'package:image/image.dart' as img;
 
 class FirebaseStorageService {
   FirebaseStorage _firebaseStorage = getIt<FirebaseStorage>();
@@ -22,7 +23,7 @@ class FirebaseStorageService {
     required String path,
   }) async {
     try {
-      metadata = SettableMetadata(contentType: 'image/jpeg');
+      metadata = SettableMetadata(contentType: 'image/png');
       final ref = _firebaseStorage.ref().child(path);
       await ref.putData(image, metadata);
 
@@ -36,5 +37,16 @@ class FirebaseStorageService {
 
   Future<void> deleteImage({required String imagePath}) async {
     await _firebaseStorage.ref().child(imagePath).delete();
+  }
+
+    Uint8List? compressAndResizeImage(Uint8List imageData){
+    final image = img.decodeImage(imageData);
+
+    if (image == null) return null;
+
+    final resized = img.copyResize(image, width: 1024, height: 1024);
+    final compressed = img.encodePng(resized);
+
+    return Uint8List.fromList(compressed);
   }
 }
