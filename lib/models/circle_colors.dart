9b90df240afac9ce5b2f0d6fe58326b8ecaaf7cb
Mateus <mateusleandro2205@gmail.com>
2024-11-20 17:29:42 -0300

    :sparkles:feat: Novas opções de cores

--- lib/models/circle_colors.dart
+++ lib/models/circle_colors.dart
@@ -2,12 +2,14 @@ import 'package:flutter/material.dart';
 
 class CircleColors {
   final Map<String, Color> _colors = const {
-    "Vermelho": Color(0xFFFF0000),
-    "Verde": Color(0xFF00FF00),
-    "Azul": Color(0xFF0000FF),
     "Amarelo": Color(0xFFFFFF00),
+    "Azul": Color(0xFF0000FF),
+    "Verde": Color(0xFF009000),
+    "Vermelho": Color(0xFFFF0000),
+    "Laranja": Color(0xFFFFA500),
+    "Rosa": Color(0xFFFFC0CB),
     "Roxo": Color(0xFF800080),
-    "Branco": Color(0xFFFFFFFF),
+    "Marrom": Color(0xFFA52A2A),
   };
 
   Map<String, Color> get currentColors => _colors;
