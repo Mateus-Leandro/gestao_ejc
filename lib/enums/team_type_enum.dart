ce1a0bc71aaee07c381ec0ef426fa37390be09f8
mateus.chagas <mateus.chagas@avancoinfo.com.br>
2025-02-25 20:56:00 -0300

    :sparkles:feat: Tela teams

--- lib/enums/team_type_enum.dart
+++ lib/enums/team_type_enum.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 enum TeamTypeEnum {
   apoioEAcolhida,
   cafezinho,
@@ -14,7 +16,7 @@ enum TeamTypeEnum {
   tiosDaExterna,
 }
 
-extension SetorExtension on TeamTypeEnum {
+extension TeamExtension on TeamTypeEnum {
   String get formattedName {
     switch (this) {
       case TeamTypeEnum.apoioEAcolhida:
@@ -43,6 +45,37 @@ extension SetorExtension on TeamTypeEnum {
         return 'Secretaria';
       case TeamTypeEnum.tiosDaExterna:
         return 'Tios da Externa';
+    }
+  }
+
+  Icon get teamIcon {
+    switch (this) {
+      case TeamTypeEnum.apoioEAcolhida:
+        return const Icon(Icons.volunteer_activism);
+      case TeamTypeEnum.cafezinho:
+        return const Icon(Icons.coffee);
+      case TeamTypeEnum.compras:
+        return const Icon(Icons.payments_outlined);
+      case TeamTypeEnum.cozinha:
+        return const Icon(Icons.soup_kitchen_outlined);
+      case TeamTypeEnum.coordenacaoGeral:
+        return const Icon(Icons.settings_accessibility);
+      case TeamTypeEnum.dirigentes:
+        return const Icon(Icons.emoji_people_outlined);
+      case TeamTypeEnum.garcoms:
+        return const Icon(Icons.room_service_outlined);
+      case TeamTypeEnum.liturgiaExterna:
+        return const Icon(Icons.book);
+      case TeamTypeEnum.liturgiaInterna:
+        return const Icon(Icons.church);
+      case TeamTypeEnum.ordemELimpeza:
+        return const Icon(Icons.clean_hands_rounded);
+      case TeamTypeEnum.salaImagens:
+        return const Icon(Icons.photo_album);
+      case TeamTypeEnum.secretaria:
+        return const Icon(Icons.monitor);
+      case TeamTypeEnum.tiosDaExterna:
+        return const Icon(Icons.people);
     }
   }
 }
