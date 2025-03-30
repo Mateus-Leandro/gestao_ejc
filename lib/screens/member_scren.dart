ad1f559f1ccbddc5d2a4ca6547000c59aa87d028
mateus.chagas <mateus.chagas@avancoinfo.com.br>
2025-03-29 23:23:10 -0300

    :sparkles:feat: Exibir membros e tios

--- lib/screens/member_scren.dart
+++ lib/screens/member_scren.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:gestao_ejc/components/forms/custom_person_form.dart';
+import 'package:gestao_ejc/components/utils/custom_list_tile.dart';
 import 'package:gestao_ejc/components/utils/custom_search_row.dart';
+import 'package:gestao_ejc/controllers/person_controller.dart';
 import 'package:gestao_ejc/models/abstract_person_model.dart';
 import 'package:gestao_ejc/screens/model_screen.dart';
+import 'package:gestao_ejc/services/locator/service_locator.dart';
 
 class MemberScren extends StatefulWidget {
   const MemberScren({super.key});
@@ -13,6 +16,15 @@ class MemberScren extends StatefulWidget {
 
 class _MemberScrenState extends State<MemberScren> {
   final TextEditingController _memberNameController = TextEditingController();
+  final PersonController _personController = getIt<PersonController>();
+  bool loadingMembers = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _personController.init();
+  }
+
   @override
   Widget build(BuildContext context) {
     return ModelScreen(
@@ -28,7 +40,8 @@ class _MemberScrenState extends State<MemberScren> {
             messageTextField: 'Pesquisar membro/tio',
             functionTextField: () => (),
             iconButton: const Icon(Icons.add),
-          )
+          ),
+          Expanded(child: _buildMembersList(context)),
         ],
       ),
       indexMenuSelected: 1,
@@ -46,5 +59,48 @@ class _MemberScrenState extends State<MemberScren> {
         );
       },
     );
+  }
+
+  Widget _buildMembersList(BuildContext context) {
+    return StreamBuilder(
+        stream: _personController.stream,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          if (snapshot.hasError) {
+            return Center(
+              child: Text('Erro ao carregar membros/tios: ${snapshot.error}'),
+            );
+          }
+
+          if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return const Center(child: Text('Nenhum membro/tio encontrado'));
+          }
+
+          var persons = snapshot.data!;
+
+          return ListView.builder(
+            itemCount: persons.length,
+            itemBuilder: (context, index) {
+              var person = persons[index];
+              return _buildMemberTile(
+                context,
+                person,
+              );
+            },
+          );
+        });
+  }
+
+  _buildMemberTile(BuildContext context, AbstractPersonModel person) {
+    return CustomListTile(
+        listTile: ListTile(
+          title: Text(person.name),
+        ),
+        defaultBackgroundColor: Colors.white);
   }
 }
